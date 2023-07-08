c4fbb9185b56a1ebfae6756d156ab9f16e37117d
Twincki <selutin.a@mail.ru>
2023-07-08 21:03:27 +0300

    обновил структуру приложения

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./components/app/App";
+import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 
-import "./styles/global.scss";
+import "./shared/styles/global.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
